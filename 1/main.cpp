769fa34c8e3381847f14819f4ea4bc48a01b1a0e
vlad1013374 <80113777+vlad1013374@users.noreply.github.com>
2024-09-03 13:48:02 +0300

    fix first lab

--- 1/main.cpp
+++ 1/main.cpp
@@ -2,23 +2,64 @@
 #include <string>
 #include <ctime>
 
-#define STRING_LENGTH 10000
 #define COUNT_LETTERS 26
 
-int main() {
-    srand(time(0));
-    std::string rand_str;
-    int letters_map[COUNT_LETTERS] = {0};
+/*
+ * Функция генерации случайной буквы
+ *
+ * @return возвращает случайную букву
+ */
+char GetRandomChar() {
+    return 'a' + rand() % COUNT_LETTERS;
+}
 
-    for (int i = 0; i < STRING_LENGTH; ++i) {
-        char rand_letter = 'a' + std::rand() % 26;
-        rand_str += rand_letter;
-        letters_map[rand_letter - 'a']++;
+/*
+ * Функция заполнения строки случайными буквами
+ *
+ * @param str - строка для заполнения
+ * @param length - длина строки
+ */
+void FillStringRandomly(std::string& str, int length) {
+    for (int i = 0; i < length; ++i) {
+        str += GetRandomChar();
     }
+}
 
+/*
+ * Функция для подсчета количества букв в строке
+ *
+ * @param str - строка для подсчета
+ * @param letters_map - массив для хранения количества букв
+ */
+void CountLetters(const std::string& str, int* letters_map) {
+    for (int i = 0; i < str.length(); ++i) {
+        letters_map[str[i] - 'a']++;
+    }
+}
+
+/*
+ * Функция для вывода результата в консоль
+ *
+ * @param letters_map - массив для хранения количества букв
+ */
+void PrintResult(int* letters_map) {
     for (char letter = 'a'; letter <= 'z'; ++letter) {
         std::cout << letter << ": " << letters_map[letter - 'a'] << std::endl;
     }
+}
 
+int main() {
+    srand(time(0));
+
+    const int STRING_LENGTH = 10000;
+
+    std::string rand_str;
+    int letters_map[COUNT_LETTERS] = {0};
+
+    FillStringRandomly(rand_str, STRING_LENGTH);
+
+    CountLetters(rand_str, letters_map);
+
+    PrintResult(letters_map);
     return 0;
 }
